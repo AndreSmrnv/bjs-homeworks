965c0f49de07607d520747741334aeab00119ba3
AndreSm <tehmir@bk.ru>
2020-09-25 13:43:35 +0300

    commit 2.3-oop-es6 correct giveBookByName

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -98,7 +98,7 @@ class StudentLog {
     }
 }
 ////tests
-
+/*
 const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
 
 console.log(sherlock.releaseDate); //2019
@@ -134,6 +134,6 @@ console.log("Количество книг после выдачи: " + library.
 for (let i of library.books) {
     console.log("книги после выдачи: " + i.name); 
 }
-/*
+
 
 */
